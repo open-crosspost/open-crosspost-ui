20cd936ecabaf0f760af5e0991dea3a093194636
Elliot Braem <elliot@ejlbraem.com>
2025-04-01 05:57:35 -0500

    add wallets

--- src/components/providers/NearWalletProvider.tsx
+++ src/components/providers/NearWalletProvider.tsx
@@ -1,5 +1,9 @@
+import { SOCIAL_CONTRACT } from "@/lib/near-social";
+import { setupBitteWallet } from "@near-wallet-selector/bitte-wallet";
+import { setupHereWallet } from "@near-wallet-selector/here-wallet";
 import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
 import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
+import { setupNearMobileWallet } from "@near-wallet-selector/near-mobile-wallet";
 import { WalletSelectorProvider } from "@near-wallet-selector/react-hook";
 import React, { ReactNode } from "react";
 import { NETWORK_ID } from "../../config";
@@ -13,8 +17,15 @@ export function NearWalletProvider({ children }: NearWalletProviderProps) {
     <WalletSelectorProvider
       config={{
         network: NETWORK_ID,
-        createAccessKeyFor: "social.near",
-        modules: [setupMyNearWallet(), setupMeteorWallet()],
+        createAccessKeyFor: SOCIAL_CONTRACT[NETWORK_ID],
+        modules: [
+          setupMyNearWallet(),
+          setupMeteorWallet(),
+          setupHereWallet(),
+          setupNearMobileWallet(),
+          // @ts-ignore
+          setupBitteWallet({ contractId: SOCIAL_CONTRACT[NETWORK_ID] }),
+        ],
       }}
     >
       {children}
