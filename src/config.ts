7ed983428d5fd3eb129aac5a6d6dc543b85415d5
Elliot Braem <elliot@ejlbraem.com>
2025-04-01 00:55:15 -0500

    use url

--- src/config.ts
+++ src/config.ts
@@ -29,7 +29,7 @@ export const EVMWalletChain = evmWalletChains[NETWORK_ID];
 
 // API Configuration
 export const OPEN_CROSSPOST_PROXY_API =
-  process.env.OPEN_CROSSPOST_PROXY_API || "http://0.0.0.0:3000";
+  "https://open-crosspost-proxy.deno.dev" || "http://0.0.0.0:3000";
 
 // Supported platforms
 export type SupportedPlatform = "Twitter" | "Near Social";
