1580ebf28b05119076ea66660f37820f56edb7c1
lokmali <malilokesh000@gmail.com>
2025-10-08 12:26:23 +0530

    Fix fastintear build issues - switch to CDN

--- src/lib/near.ts
+++ src/lib/near.ts
@@ -1,6 +1,2 @@
-import * as FastIntear from "fastintear";
-
-export const near: typeof FastIntear =
-  typeof window !== "undefined"
-    ? window.near // in browser
-    : FastIntear; // on server
+// Using global window.near from CDN
+export const near = typeof window !== "undefined" ? window.near : undefined;
